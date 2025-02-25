a51f24d0642599784042d9fa71f0e53951ec624b
LeoGMT <garcia.mata.leonardo1@gmail.com>
2025-02-24 20:19:45 -0600

    Update Validacion.js

--- ValidarEtiquetas/Validacion.js
+++ ValidarEtiquetas/Validacion.js
@@ -1,3 +1,7 @@
-function validar(){
+function sanitizarHtml() {
+    let input = document.getElementById("inputHtml").value;
+
+    let sanitizedHtml = input.replace(/<\/?(script|marquee|iframe|object|embed|style|link|applet|blink|form|input|textarea|button|select|option|)[^>]*>/gi, "");
     
+    document.getElementById("outputHtml").innerHTML = sanitizedHtml;
 }
