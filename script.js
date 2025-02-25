74afbeb65dc98d21a969638c474669e5b21f3855
LeoGMT <garcia.mata.leonardo1@gmail.com>
2025-02-24 20:36:40 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-function sanitizarHtml() {
+function verificarHtml() {
     let input = document.getElementById("inputHtml").value;
+    let parser = new DOMParser();
+    let doc = parser.parseFromString(input, "text/html");
 
-    let sanitizedHtml = input.replace(/<\/?(script|marquee|iframe|object|embed|style|link|applet|blink|form|input|textarea|button|select|option|)[^>]*>/gi, "");
-    
-    document.getElementById("outputHtml").innerHTML = sanitizedHtml;
+    // Verifica si hay alguna etiqueta en el contenido
+    if (doc.body.children.length > 0) {
+        alert("NEEEEEEEEEEL PERRO tu no pasas");
+    } else {
+        alert("Texto fino de traje elegante apto para pasar");
+    }
 }
